45c2d9805e6b9de3ff321ac6591a23766fb3b0d0
abenteuerzeit <dr.adrian.mroz@outlook.com>
2023-12-13 10:32:46 +0100

    initial commit

---
+++ function_app.py
@@ -0,0 +1,11 @@
+import logging
+import azure.functions as func
+
+
+app = func.FunctionApp()
+
+@app.event_hub_message_trigger(arg_name="azeventhub", event_hub_name="myeventhub",
+                               connection="EventHubConnectionString") 
+def eventhub_trigger(azeventhub: func.EventHubEvent):
+    logging.info('Python EventHub trigger processed an event: %s',
+                azeventhub.get_body().decode('utf-8'))
