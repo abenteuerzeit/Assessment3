6e270dc1fc6a3af64d36a0d402f1f05dd257237e
abenteuerzeit <dr.adrian.mroz@outlook.com>
2023-12-13 10:43:31 +0100

    refactor: add docstrings

--- function_app.py
+++ function_app.py
@@ -1,11 +1,21 @@
+"""function_app.py"""
+
 import logging
 import azure.functions as func
 
 
 app = func.FunctionApp()
 
-@app.event_hub_message_trigger(arg_name="azeventhub", event_hub_name="myeventhub",
-                               connection="EventHubConnectionString") 
-def eventhub_trigger(azeventhub: func.EventHubEvent):
-    logging.info('Python EventHub trigger processed an event: %s',
-                azeventhub.get_body().decode('utf-8'))
+
+@app.event_hub_message_trigger(arg_name="azeventhub",
+                               event_hub_name="myeventhub",
+                               connection="EventHubConnectionString")
+def eventhub_trigger(azeventhub: func.EventHubEvent) -> None:
+    """_summary_
+    Event hub trigger function that logs the events it receives.
+
+    Args:
+        azeventhub (func.EventHubEvent): _description_
+    """
+    event = azeventhub.get_body().decode('utf-8')
+    logging.info('Python EventHub trigger processed an event: %s', event)
